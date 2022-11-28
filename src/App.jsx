6407778a249f49ab555f75605dadb7d60e0704d7
carraes <carraeshb@gmail.com>
2022-11-27 21:22:40 -0300

    fix: some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState, useEffect } from 'react'
-import Header from "./components/Header"
-import Form from "./components/Form"
-import Card from "./components/Card"
-import Preview from "./components/Preview"
-import Footer from "./components/Footer"
-import { data as cardData } from './data/data'
-
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import Card from './components/Card';
+import Preview from './components/Preview';
+import Footer from './components/Footer';
+import { data as cardData } from './data/data';
 
 function App() {
   const defineRarity = () => {
-    const rarity = (Math.floor(Math.random() * 100))+1;
-    if (rarity <= 60) return 'normal'
-    else if (rarity <= 80) return 'rare'
-    else if (rarity <= 94) return 'epic'
-    else if (rarity <= 100) return 'legendary'
+    const rarity = Math.floor(Math.random() * 100) + 1;
+    if (rarity <= 60) return 'normal';
+    else if (rarity <= 80) return 'rare';
+    else if (rarity <= 94) return 'epic';
+    else if (rarity <= 100) return 'legendary';
   };
 
-  const rarityData = cardData.map((card) => ({...card, rarity: defineRarity()}))
+  const rarityData = cardData.map((card) => ({ ...card, rarity: defineRarity() }));
 
   const [data, setData] = useState({
     name: '',
@@ -35,39 +34,28 @@ function App() {
 
   const validateForm = () => {
     const { name, hp, mana, attack, armor, speed, image } = data;
-    if (name && hp && mana && attack && armor && speed && image) { 
+    if (name && hp && mana && attack && armor && speed && image) {
       setSbmtBtn(false);
     } else {
       setSbmtBtn(true);
     }
   };
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     validateForm();
-  }, [data])
+  }, [data]);
 
   const handleChange = (e) => {
     const { name, value, checked, type } = e.target;
     setData((prevData) => ({
       ...prevData,
       [name]: type === 'checkbox' ? checked : value,
-    }))
-    
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { 
-      name,
-      hp,
-      mana,
-      attack,
-      armor,
-      speed,
-      rarity,
-      image,
-      query,
-    } = data;
+    const { name, hp, mana, attack, armor, speed, rarity, image, query } = data;
 
     const newEntry = {
       name,
@@ -77,70 +65,84 @@ function App() {
       rarity,
       armor,
       speed,
-      image, 
+      image,
       fromSubmit: true,
-    }
+    };
 
-    setData((prevData) => ({...prevData , saved: [...query, newEntry] ,query: [...query, newEntry]}))
+    setData((prevData) => ({
+      ...prevData,
+      saved: [...query, newEntry],
+      query: [...query, newEntry],
+    }));
   };
 
-  
   const handleFilter = (e) => {
     const { value } = e.target;
     const { query } = data;
-    const filteredSearch = query.filter((card) => card.name.includes(value));
-    setData(prevState => ({...prevState, saved: filteredSearch}))
+    const filteredSearch = query.filter((card) =>
+      card.name.toLowerCase().includes(value.toLowerCase()),
+    );
+    setData((prevState) => ({ ...prevState, saved: filteredSearch }));
   };
-  
+
   const handleRarityFilter = (e) => {
     const { value } = e.target;
     const { query } = data;
     const filteredSearch = query.filter((card) => card.rarity === value);
-    if (value === 'todos'){
-      setData(prevState => ({...prevState, saved: query}))
+    if (value === 'todos') {
+      setData((prevState) => ({ ...prevState, saved: query }));
     } else {
-      setData(prevState => ({...prevState, saved: filteredSearch}))
+      setData((prevState) => ({ ...prevState, saved: filteredSearch }));
     }
-  }
-  
+  };
+
   const handleDelete = (e, str) => {
     const { query } = data;
-    const newQuery = query.filter((card) => card.name !== str)
-    setData(prevState => ({...prevState, saved: newQuery, query: newQuery}))
-  }
+    const newQuery = query.filter((card) => card.name !== str);
+    setData((prevState) => ({ ...prevState, saved: newQuery, query: newQuery }));
+  };
 
   const handleSent = (e) => {
     const { query } = data;
     const { checked } = e.target;
     let newQuery = [];
     if (checked) {
-      newQuery = query.filter((card) => card.fromSubmit === checked)
+      newQuery = query.filter((card) => card.fromSubmit === checked);
     } else {
-      newQuery = query.filter((card) => card.fromSubmit !== checked)
+      newQuery = query.filter((card) => card.fromSubmit !== checked);
     }
-    setData(prevState => ({...prevState, saved: newQuery}))
+    setData((prevState) => ({ ...prevState, saved: newQuery }));
   };
 
   const { saved } = data;
 
-  const sortedSave = saved.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
+  const sortedSave = saved.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
 
   return (
     <>
       <Header />
-      <section className='flex justify-center items-center gap-12 flex-col md:flex-row md:justify-evenly' >  
+      <section className='flex justify-center items-center gap-12 flex-col md:flex-row md:justify-evenly'>
         <Form
           data={data}
           handleChange={handleChange}
           handleSubmit={handleSubmit}
           sbmtBtn={sbmtBtn}
         />
-        <Card data={data} handleDelete={handleDelete}/>
+        <Card
+          data={data}
+          handleDelete={handleDelete}
+        />
       </section>
-      <Preview sortedData={sortedSave} handleFilter={handleFilter} handleRarityFilter={handleRarityFilter} handleDelete={handleDelete} handleSent={handleSent} />
+      <Preview
+        sortedData={sortedSave}
+        handleFilter={handleFilter}
+        handleRarityFilter={handleRarityFilter}
+        handleDelete={handleDelete}
+        handleSent={handleSent}
+      />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,37 +1,63 @@
-import Card from './Card'
+import Card from './Card';
 
-const Preview = ({sortedData, handleFilter, handleRarityFilter, handleDelete, handleSent}) => {
+const Preview = ({ sortedData, handleFilter, handleRarityFilter, handleDelete, handleSent }) => {
   return (
-    <section className="flex flex-col justify-between gap-4 mt-6">
+    <section className='flex flex-col justify-between gap-4 mt-6'>
       {/* Inptus */}
-      <section className="flex justify-center items-center flex-col gap-4 md:flex-row md:justify-evenly">
-        <div className="space-x-2">
-          <span className="text-md self-center">Filtros:</span>
-          <input type="text" className="form-input" name='filtroName' onChange={handleFilter} />
+      <section className='flex justify-center items-center flex-col gap-4 md:flex-row md:justify-evenly'>
+        <div className='space-x-2'>
+          <span className='text-md self-center'>Filtros:</span>
+          <input
+            type='text'
+            className='form-input'
+            name='filtroName'
+            onChange={handleFilter}
+          />
         </div>
-        <label htmlFor="filtroRaridade" className="space-x-2 self-center">
+        <label
+          htmlFor='filtroRaridade'
+          className='space-x-2 self-center'
+        >
           <span>Raridade:</span>
-          <select name="filtroRaridade" id="filtroRaridade" onChange={handleRarityFilter} >
-            <option value="todos">Todos</option>
-            <option value="normal">Normal</option>
-            <option value="rare">Raro</option>
-            <option value="epic">Epico</option>
-            <option value="legendary">Lendario</option>
+          <select
+            name='filtroRaridade'
+            id='filtroRaridade'
+            onChange={handleRarityFilter}
+          >
+            <option value='todos'>Todos</option>
+            <option value='normal'>Normal</option>
+            <option value='rare'>Raro</option>
+            <option value='epic'>Epico</option>
+            <option value='legendary'>Lendario</option>
           </select>
         </label>
-        <label htmlFor="filterTrunfo" className="space-x-2 self-center">
-          <input type="checkbox" id="filterTrunfo" className="input-checkbox self-center focus:ring-0" onChange={handleSent} />
+        <label
+          For='filterTrunfo'
+          className='space-x-2 self-center cursor-pointer'
+        >
+          <input
+            type='checkbox'
+            id='filterTrunfo'
+            className='input-checkbox self-center focus:ring-0 cursor-pointer'
+            onChange={handleSent}
+          />
           <span>Enviada pelo usuário</span>
         </label>
       </section>
       {/* Preview */}
       <section className='flex flex-wrap justify-center gap-4 p-6'>
-        {sortedData.map((card)=> {
-          return <Card key={card.name} data={card} handleDelete={handleDelete} />
+        {sortedData.map((card) => {
+          return (
+            <Card
+              key={card.name}
+              data={card}
+              handleDelete={handleDelete}
+            />
+          );
         })}
       </section>
-    </section>  
-  )
-}
+    </section>
+  );
+};
 
-export default Preview
+export default Preview;
